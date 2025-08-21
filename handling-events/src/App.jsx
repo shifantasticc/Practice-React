29b6645179ac84756f614588cd8f9472b628061e
shifantasticc <shifastudyit@gmail.com>
2025-08-21 22:07:54 +0530

    Adding App files

---
+++ handling-events/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import Button from './Button';
+import Form from './Form';
+
+function App() {
+  return (
+    <>
+      <Form />
+      <br />
+      <Button />
+    </>
+  );
+}
+
+export default App;
